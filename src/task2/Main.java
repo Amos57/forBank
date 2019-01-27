92c628591705ba146fc5017bfdbe7e73f03c43de
Олег <31664872+Amos57@users.noreply.github.com>
2019-01-27 15:48:26 +0300

    Update Main.java

--- src/task2/Main.java
+++ src/task2/Main.java
@@ -1,10 +1,8 @@
 package task2;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,13 +21,6 @@ import task1.Deal;
 public class Main {
 
 	/**
-	 * переменные для использования в лямбда-выражениях
-	 * 
-	 */
-	static int count = 1;
-	static Date key;
-
-	/**
 	 * Напишите программу, которая считает статистику по операциям. Данные об
 	 * операциях находятся в файле, который сгенерирован в предыдущей задаче.
 	 * Программа должна подсчитать: -сумму всех операций за каждый день -суммы
@@ -40,14 +31,13 @@ public class Main {
 	 * Статистика по датам должна быть отсортирована по возрастанию дат.
 	 * Статистика по точкам продаж должна быть отсортирована по убыванию суммы.
 	 * 
-	 * Пример запуска программы: java -jar task2.jar operations.txt
-	 * sums-by-dates.txt sums-by-offices.txt
+	 * Пример запуска программы: 
+	 * java -jar task2.jar operations.txt sums-by-dates.txt sums-by-offices.txt
+	 *
 	 * 
 	 * @param args
-	 * @throws ParseException
-	 * @throws IOException
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args){
 
 		String data;
 		String sumsByDates;
@@ -55,30 +45,25 @@ public class Main {
 
 		  if (args.length < 3) {
 		  System.out.println("параметры для работы программы не заданы");
-		  return; }
-		  
+		  return; 
+		  }
+		
 		  data = args[0]; sumsByDates = args[1]; sumsByOffices = args[2];
-		 
 
-		try {
-
-			//используем формат времени без часов, поскольку работаем с датами
+			// используем формат времени без часов и минут, поскольку работаем с датами
 			SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
 			// список сделок
-			List<Deal> deals;
-			
-			try(Stream<String> s = Files.lines(//для закрытия ресурсов
-					Paths.get(data),
-					StandardCharsets.UTF_8)){
+			List<Deal> deals = null;
 
-			 deals = s.flatMap(str -> Stream.of(str.split("\n")))
-					.map(Deal::getDeal).collect(Collectors.toList());
-			}
-			
-			
-			
-			//сортировка времени по убыванию		
+			try (Stream<String> s = Files.lines(// для автомат. закрытия ресурсов используем try с ресурсами
+					Paths.get(data),StandardCharsets.UTF_8)) {
+				    deals = s.flatMap(str -> Stream.of(str.split("\n")))
+						.map(Deal::getDeal).collect(Collectors.toList());
+				    
+	
+
+			// сортировка времени по возрастанию дат
 			Comparator<Date> comByDate = (v1, v2) -> {
 				if (v1.getTime() > v2.getTime())
 					return 1;
@@ -87,70 +72,63 @@ public class Main {
 				return 0;
 			};
 
-			// карта:  дата сделок , значение колличество сделок
+			// карта: key: дата сделок, value: колличество сделок
 			Map<Date, Integer> byDates = new TreeMap<>(comByDate);
+			
+			// карта: key: номер офиса, value: колличество сделок
 			Map<Integer, Integer> byOffices = new TreeMap<>();
 
-			deals.stream().forEach((e) -> {
-				if (!byOffices.containsKey(e.getNumberOffice()))
-					byOffices.put(e.getNumberOffice(), count);
-				else {
-					count = byOffices.get(e.getNumberOffice());
-					byOffices.put(e.getNumberOffice(), count++);
-				}
+			deals.stream().forEach((e) -> {	
+						byOffices.merge(e.getNumberOffice(), 1, (v1, v2) -> v1+ v2);
+						try {
+						byDates.merge(dateFormat.parse(e.getDate()), 1, (v1, v2) -> v1 + v2);		
+						} catch (Exception e2) {
+							 System.out.println("Не корректная дата в файле "+ data);
+							 e2.printStackTrace();
+						}
+					});
 
-				try {
-					key = dateFormat.parse(e.getDate());
-					if (!byDates.containsKey(key))
-						byDates.put(key, count);
-					else {
-						count = byDates.get(key);
-						byDates.put(key, count++);
-					}
-				} catch (Exception e2) {
-					 System.out.println("Не корректная дата в файле " + data);
-				}
+			//Добавляем в лист для дальнейшей сортировки номеров точек по убыванию
+			List<Entry<Integer, Integer>> listOffice = new ArrayList<>(byOffices.entrySet());
+					
+			//Сортируем, добавив компоратор.Соортировка по убыванию
+			Collections.sort(listOffice, (v1, v2) -> {return v2.getValue() - v1.getValue();});
+				
+			
 
-			});
-
-			List<Entry<Integer, Integer>> listOffice = new ArrayList<>(
-					byOffices.entrySet());
-			Collections.sort(listOffice, (v1, v2) -> {
-				return v2.getValue() - v1.getValue();
-			});
-
+			//Формируем данные по средству StringBuilder
 			StringBuilder fileSumsByOffices = new StringBuilder();
+			
 			fileSumsByOffices.append("Кол операций  Номер точки");
 			fileSumsByOffices.append("\r\n");
-			listOffice.stream().forEach(
-					(entr) -> {
-						fileSumsByOffices.append(entr.getValue() + " "
-								+ entr.getKey());
+			
+			listOffice.stream().forEach((entr) -> {
+						fileSumsByOffices.append(entr.getValue() + " "+ entr.getKey());	
 						fileSumsByOffices.append("\r\n");
 					});
 
 			Set<Entry<Date, Integer>> setDate = byDates.entrySet();
 			StringBuilder fileSumsByDates = new StringBuilder();
+			
 			fileSumsByDates.append("Дата   Колл операций");
 			fileSumsByDates.append("\r\n");
-			setDate.stream().forEach(
-					(entr) -> {
-						fileSumsByDates.append(dateFormat.format(entr.getKey())
-								+ " " + entr.getValue());
+			
+			setDate.stream().forEach((entr) -> {
+						fileSumsByDates.append(dateFormat.format(entr.getKey())+ " " + entr.getValue());
 						fileSumsByDates.append("\r\n");
 					});
 
+			   //Выводим в файл
+			    Files.write(Paths.get(sumsByDates), fileSumsByDates.toString().getBytes());
+			    Files.write(Paths.get(sumsByOffices),fileSumsByOffices.toString() .getBytes());
+			  
+			  }catch(Exception e  ){
+				  e.printStackTrace();
+			  }
+			 
 
-
-			   Files.write(Paths.get(sumsByDates), fileSumsByDates.toString()
-			   .getBytes());
-			   Files.write(Paths.get(sumsByOffices),
-			   fileSumsByOffices.toString()
-			   .getBytes());
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 }
+
